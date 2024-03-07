693b49ea6ebd49bc69e642ef07f3a90efdcef7d1
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-03-07 12:01:56 +0530

    Add files via upload

---
+++ input_1.py
@@ -0,0 +1,5 @@
+# Formula use add two values
+a=int(input("Enter a value: "))        # Take a value to user & convert int
+b=int(input("Enter b value: "))        # Take b value to user & convert int
+c=a+b                                  # This formula to add two values
+print("The sum is",c)                  # print execute the formula or sum value
